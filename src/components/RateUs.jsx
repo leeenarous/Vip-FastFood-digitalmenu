15769669596d96b70433bb1df69aeefd3934ebae
Leen Arous <leenarous4@.com>
2026-06-29 11:31:12 +0300

    Update menu

--- src/components/RateUs.jsx
+++ src/components/RateUs.jsx
@@ -16,7 +16,7 @@ export default function RateUs() {
         <p>تقييمك بيهمنا</p>
 
         <a
-          href="https://m.facebook.com/vip.tartus/reviews/"
+          href="https://www.facebook.com/vip.tartus/reviews/?sk=reviews"
           target="_blank"
           rel="noopener noreferrer"
           className="rate-cta"
